63966aa0190c2a60ff917f8d745d55c067f4f06f
Alfred Broderick <alfredbr@outlook.com>
2025-01-26 00:45:03 +0100

    Add loop to increment counter and update string length calculation in assembly

--- asm/3-numbers/numbers.s
+++ asm/3-numbers/numbers.s
@@ -5,14 +5,18 @@
 .include "macros.s"                   // Include the macros file
 
 _start:
-    //ldr x1, =msg                    // Load the address of msg into x1
-    //bl print_string                 // Call print_string
-
-    ldr x0, =1234567890               // Load the integer 1234567890 into x0
+    ldr x1, =msg                    // Load the address of msg into x1
+    bl print_string                 // Call print_string
+init:
+	mov x20, #0
+counter:
+    mov x0, x20                       // Load the integer #0 into x0
     ldr x1, =buffer                   // Load the address of buffer into x1
     bl itoa                           // Convert the integer to ASCII
     bl print_string                   // Call print_string
-
+	add x20, x20, #1				  // Increment x20
+	cmp x20, #100					  // Compare x20 with #100
+	b.lt counter				      // If x20 < 100, continue the loop
 _exit:
     mov x0, #0                        // Set x0 to 0 (successful exit status)
     mov x8, #93                       // Set x8 to 93 (sys_exit syscall number)
@@ -78,10 +82,10 @@ strlen:
 strlen_loop:
     ldrb w4, [x1, x6]                 // Load the byte at the address x1 + x6 into w4
     cmp w4, #0                        // Compare the byte in w4 with 0x0 null terminator
-    beq strlen_end                    // If null detected, branch to strlen_end
+    beq strlen_done                   // If null detected, branch to strlen_done
     add x6, x6, #1                    // Otherwise, increment length in x6
     b strlen_loop                     // We're not done, go back to top of loop
-strlen_end:
+strlen_done:
     mov x2, x6                        // Move the length from x6 to x2
     epilogue
     ret

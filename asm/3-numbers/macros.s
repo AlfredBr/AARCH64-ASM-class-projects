f84c4930f12e230662015e91f336df10a6b1af6c
Alfred Broderick <alfredbr@outlook.com>
2025-01-25 17:39:06 +0100

    first

---
+++ asm/3-numbers/macros.s
@@ -0,0 +1,14 @@
+// Define the prologue macro
+.macro prologue
+    stp x29, x30, [sp, #-16]!    // Save x29 and x30
+    mov x29, sp                  // Set frame pointer
+.endm
+
+// Define the epilogue macro
+.macro epilogue
+    ldp x29, x30, [sp], #16      // Restore x29 and x30
+.endm
+
+// Constants
+.equ NEWLINE, 10
+.equ NULL_TERMINATOR, 0

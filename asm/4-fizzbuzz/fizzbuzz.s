976607d7db90450b74e25b86bfc539f54420cc0f
Alfred Broderick <alfredbr@outlook.com>
2025-01-26 19:37:54 +0100

    remove x6 useage from strlen

--- asm/4-fizzbuzz/fizzbuzz.s
+++ asm/4-fizzbuzz/fizzbuzz.s
@@ -103,15 +103,14 @@ print_string:
 
 strlen:
     prologue
-    mov  x6, #0                       // Initialize x6 to 0
+    mov  x2, #0                       // Initialize x6 to 0
 strlen_loop:
-    ldrb w4, [x1, x6]                 // Load the byte at the address x1 + x6 into w4
+    ldrb w4, [x1, x2]                 // Load the byte at the address x1 + x6 into w4
     cmp  w4, #0                       // Compare the byte in w4 with 0x0 null terminator
     beq  strlen_done                  // If null detected, branch to strlen_done
-    add  x6, x6, #1                   // Otherwise, increment length in x6
+    add  x2, x2, #1                   // Otherwise, increment length in x6
     b    strlen_loop                  // We're not done, go back to top of loop
 strlen_done:
-    mov  x2, x6                       // Move the length from x6 to x2
     epilogue
     ret
 

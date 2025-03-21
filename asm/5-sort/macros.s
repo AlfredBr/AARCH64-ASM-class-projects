d64cb9485cad63f2609b59b9e09fc25ce53b4502
Alfred Broderick <alfredbr@outlook.com>
2025-03-21 04:03:29 +0100

    update

--- asm/5-sort/macros.s
+++ asm/5-sort/macros.s
@@ -1,4 +1,4 @@
-// prologue: set up frame pointer and link register
+// prologue: save frame pointer and link register
 .macro prologue
     stp     x29, x30, [sp, #-16]!          // Save frame pointer and link register
     mov     x29, sp                        // Set frame pointer to stack pointer
@@ -9,16 +9,24 @@
     ldp     x29, x30, [sp], #16            // Restore frame pointer and link register
 .endm
 
-// push: store registers on the stack
+// push: store registers on the stack using pre-decrement addressing mode
 .macro push reg1, reg2
-    sub     sp, sp, #16                   // Allocate space for two registers
-    stp     \reg1, \reg2, [sp]            // Store registers in memory
+    stp     \reg1, \reg2, [sp, #-16]!      // Pre-decrement sp and then store registers
 .endm
 
-// pop: restore registers from the stack
+// pop: restore registers from the stack using post-increment addressing mode
 .macro pop reg1, reg2
-    ldp     \reg1, \reg2, [sp]            // Load registers from memory
-    add     sp, sp, #16                   // Restore stack pointer
+    ldp     \reg1, \reg2, [sp], #16        // Load registers and post-increment sp by 16
+.endm
+
+// Macro to allocate space on the stack ensuring 16-byte alignment
+.macro stack_alloc size
+    sub     sp, sp, \size
+.endm
+
+// Macro to free space on the stack
+.macro stack_free size
+    add     sp, sp, \size
 .endm
 
 // constants
